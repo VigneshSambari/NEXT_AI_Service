5761c01ff70f1d854ba0846e8dd6492eb2638d2d
VickySam1901 <72963230+VickySam1901@users.noreply.github.com>
2023-09-02 16:38:05 +0530

    generations done

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,9 +18,13 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <html lang="en">
+          <body className={inter.className}>{children}</body>
+        </html>
+      
     </ClerkProvider>
   )
 }
